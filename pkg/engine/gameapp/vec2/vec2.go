c6712682cb686e9059be95b6f502fe1d739096f5
kulibaba.mihail <kulibaba.mihail@wildberries.work>
2023-12-31 14:46:08 +0300

    added vec2 utils

--- pkg/engine/gameapp/vec2/vec2.go
+++ pkg/engine/gameapp/vec2/vec2.go
@@ -5,3 +5,23 @@ import "cmp"
 type Vector[T cmp.Ordered] struct {
 	X, Y T
 }
+
+func NewVector[T cmp.Ordered](x, y T) *Vector[T] {
+	return &Vector[T]{x, y}
+}
+
+func (v *Vector[T]) WithX(value T) *Vector[T] {
+	v.X = value
+	return v
+}
+
+func (v *Vector[T]) WithY(value T) *Vector[T] {
+	v.Y = value
+	return v
+}
+
+func (v *Vector[T]) WithXY(x, y T) *Vector[T] {
+	v.X = x
+	v.Y = y
+	return v
+}
